42e34c03b8f42c50f97473f52c79012f5118692a
Karan <karanprince85@gmail.com>
2025-01-03 22:24:02 +0530

    tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Job-Portal-Website</div>
+    <div>
+      <h1 className='text-green-400'>Job-Portal-Website</h1>
+    </div>
   )
 }
 
