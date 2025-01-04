db24f4b3b7fab53f7817c8df84ade3d912db158b
Dyuti Agarwal <dyuti.z.agarwal@gmail.com>
2025-01-04 22:32:00 +0530

    Job lists Version1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import PostLists from "./components/PostLists";
 
 const App = () => {
-  return (
-    <div>
-      <h1 className='text-green-400'>Job-Portal-Website</h1>
-    </div>
-  )
-}
+	return (
+		<>
+			<PostLists />
+		</>
+	);
+};
 
-export default App
+export default App;
