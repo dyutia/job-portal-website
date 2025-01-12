e4366b9529589920d49858f42e12b4782c217aec
Dyuti Agarwal <dyuti.z.agarwal@gmail.com>
2025-01-12 10:01:54 +0530

    Clean up

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,11 @@ import React, { useState } from "react";
 import JobLists from "./components/JobLists";
 import PostJob from "./components/PostJob";
 import JobListProvider from "./store/job-lists-store";
-import ApplyForm from "./components/ApplyForm";
-import Header from "./components/Header";
 import Filter from "./components/Filter";
+import JobHeader from "./components/JobHeader";
 
 const App = () => {
-	const [selectedTab, setSelectedTab] = useState("Jobs");
-	return (
-		<JobListProvider>
-			<Header />
-			{selectedTab === "Jobs" && (
-				<div className="flex m-24 justify-center">
-					<Filter />
-					<JobLists />
-				</div>
-			)}
-			{selectedTab === "PostJob" && <PostJob />}
-		</JobListProvider>
-	);
+	return <JobListProvider></JobListProvider>;
 };
 
 export default App;
--- src/components/JobHeader.jsx
+++ src/components/JobHeader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Header = () => {
+const JobHeader = () => {
 	return <div className="bg-red-400  h-80">top</div>;
 };
 
-export default Header;
+export default JobHeader;
